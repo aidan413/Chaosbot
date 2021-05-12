2da491796a94462127d6f2a7a9b553b733d00504
aidan413 <aidan.herz.igen@gmail.com>
2021-05-12 10:24:25 -0400

    all

--- bot.py
+++ bot.py
@@ -13,35 +13,82 @@ def read_token():
 
 token=read_token()
 invdict={}
+randict={}
 
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
         invdict[guild.id]="a"
+        randict[guild.id]=False
     print(f'We have logged in as {bot.user}')
 
 @bot.event
 async def on_guild_join(guild):
     invdict[guild.id]="a"
+    randict[guild.id]=False
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if (invdict[message.guild.id] in message.content.lower() and not(message.content.startswith('#c ') and not(' ' in message.content[3:]))):
-        await message.channel.send('no')
-        await message.delete()
+    is_command=message.content.startswith('#') and message.content[1].isalpha()
+
+    for i in invdict[message.guild.id]:
+        if (i in message.content.lower() and not is_command):
+            if randict[message.guild.id]==True:
+                for letter in reversed(message.content):
+                    if letter.isalpha():
+                        invdict[message.guild.id]=letter.lower()
+                        break
+                    
+            await message.channel.send('no')
+            await message.delete()
 
     await bot.process_commands(message)
         
 @bot.command(name='c')
 async def change(ctx, args):
-    if not(args[0].isalpha()):
+    if not(str(args).isalpha()):
         return
     
-    invdict[ctx.guild.id]=(str(args[0]))
-    await ctx.send('done')
+    invdict[ctx.guild.id]=(str(args))
+    embed=discord.Embed(
+        title='Key is now '+str(args),
+        color=discord.Colour.red()
+    )
+    await ctx.send(embed=embed)
+
+@bot.command(name='k')
+async def key(ctx):
+    embed=discord.Embed(
+        title='Key is currently '+invdict[ctx.guild.id],
+        color=discord.Colour.red()
+    )
+    await ctx.send(embed=embed)
+
+@bot.command(name='rt')
+async def random_on(ctx):
+    
+    randict[ctx.guild.id]=True
+    embed=discord.Embed(
+    title='Random key change on',
+    color=discord.Colour.red()
+    )
+    await ctx.send(embed=embed)
+    
+
+@bot.command(name='rf')
+async def random_off(ctx):
+    randict[ctx.guild.id]=False
+    embed=discord.Embed(
+    title='Random key change off',
+    color=discord.Colour.red()
+    )
+    await ctx.send(embed=embed)
+
+
+
 
 
 
